49b3e13427101b2c83e5d7108d243f9f3bcd8641
unknown <germain.ruget@gmail.com>
2020-04-18 10:49:22 +0200

    Basic move + carrying

---
+++ Assets/Levain/Collectibles/Collectibles.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum collectibleType{
+   None,
+   Flour,
+   Water,
+}
+public class Collectibles : MonoBehaviour {
+   public collectibleType type; 
+
+   // Start is called before the first frame update
+   void Start() {
+
+   }
+
+   // Update is called once per frame
+   void Update() {
+
+   }
+}
--- Assets/Levain/Player/PlayerMovement.cs
+++ Assets/Levain/Player/PlayerMovement.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+enum TransitionParameter {
+   Move,
+   Holding,
+}
+
 public class PlayerMovement : MonoBehaviour {
+   public GameObject Player;
 
    private PlayerInputActions inputActions;
    private Vector2 movementInput;
+   private bool IsMoving;
 
    [SerializeField]
    private float moveSpeed = 10f;
@@ -13,11 +20,29 @@ public class PlayerMovement : MonoBehaviour {
    private Vector3 inputDirection;
    private Vector3 moveVector;
    private Quaternion currentRotation;
+   private Animator animator;
+
+   [Header("Picking Up")]
+   public bool canPickUp;
+   public GameObject Pickable;
+   [Header("Holding")]
+   public bool isHolding;
+   public GameObject Holding;
+
+   void Start() {
+      Player = gameObject;
+   }
 
    void Awake() {
       inputActions = new PlayerInputActions();
       inputActions.Player.Movement.performed += context => movementInput = context.ReadValue<Vector2>();
 
+      animator = GetComponentInChildren<Animator>();
+   }
+
+   void Update() {
+      if (canPickUp && !isHolding)PickUp();
+      else if (isHolding)PutDown();
    }
 
    void FixedUpdate() {
@@ -35,7 +60,12 @@ public class PlayerMovement : MonoBehaviour {
 
       Vector3 desiredDirection = camForward * inputDirection.z + camRight * inputDirection.x;
 
-      Move(desiredDirection);
+      if (Mathf.Abs(h) > 0 || Mathf.Abs(v) > 0) {
+         Move(desiredDirection);
+         animator.SetBool(TransitionParameter.Move.ToString(), true);
+      } else {
+         animator.SetBool(TransitionParameter.Move.ToString(), false);
+      }
 
       Turn(desiredDirection);
    }
@@ -61,5 +91,51 @@ public class PlayerMovement : MonoBehaviour {
 
    private void OnDisable() {
       inputActions.Disable();
+   }
+
+   private void OnTriggerEnter(Collider other) {
+      Collectibles collectible = other.GetComponent<Collectibles>();
+      if (collectible != null) {
+         Pickable = collectible.gameObject;
+         canPickUp = true;
+      }
+   }
+
+   private void OnTriggerExit(Collider other) {
+      Pickable = null;
+      canPickUp = false;
+   }
+
+   private void PickUp() {
+      if (Input.GetKeyDown(KeyCode.E) && Pickable != null) {
+         Holding = Pickable;
+         Pickable.SetActive(false);
+         isHolding = true;
+         animator.SetBool(TransitionParameter.Holding.ToString(), true);
+
+         //Remove pickable
+         Pickable = null;
+         canPickUp = false;
+
+         //Slow Down player
+         moveSpeed = 4f;
+      }
+   }
+
+   private void PutDown() {
+      if (Input.GetKeyDown(KeyCode.E) && Holding != null) {
+         //Place Object
+         Holding.transform.position = ((Player.transform.forward * 1f) + Player.transform.position) + new Vector3(0, 2, 0);
+         Holding.SetActive(true);
+
+         //Let Go
+         Holding = null;
+         isHolding = false;
+         animator.SetBool(TransitionParameter.Holding.ToString(), false);
+
+         //Put back player speed;
+         moveSpeed = 8f;
+
+      }
    }
 }
